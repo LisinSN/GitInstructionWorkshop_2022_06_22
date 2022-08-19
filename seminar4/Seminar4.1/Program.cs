b8657e18213e8d14998f565cfb7fac727469a52c
Лисин Сергей Николаевич <LisinSN@sklif-b.ru>
2022-08-19 13:53:16 +0300

    Семинар1 Задание1 Квадрат введённого числа

---
+++ seminar4/Seminar4.1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Семинар 1. Задача 1. Программа ввода и нахождени квадрата числа
+int num;
+// ввод данных
+Console.Write ("Введите число: ");
+num = Convert.ToInt32(Console.ReadLine());
+//вычисление квадрата
+int square = num * num;
+//вывод ответа на экран
+Console.WriteLine ("Квадрат числа равен: "+square);
+
